0df10040b35a4040c96e86515788e606edd5d38e
Joel <joeldmyers@gmail.com>
2020-05-27 08:53:14 -0400

    mdx setup and first mdx page

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,5 +3,16 @@ module.exports = {
     title: "My Gatsby Blog",
     description: "A demo blog site",
   },
-  plugins: ["gatsby-plugin-emotion", "gatsby-plugin-react-helmet"],
+  plugins: [
+    "gatsby-plugin-emotion",
+    "gatsby-plugin-react-helmet",
+    {
+      resolve: "gatsby-plugin-mdx",
+      options: {
+        defaultLayouts: {
+          default: require.resolve("./src/components/layout.js"),
+        },
+      },
+    },
+  ],
 };
