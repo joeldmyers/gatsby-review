5ab9352cc9dfd7206b3565d2617da6dd2bbe5202
Joel <joeldmyers@gmail.com>
2020-05-26 23:20:35 -0400

    some tweaks

--- src/components/header.js
+++ src/components/header.js
@@ -9,10 +9,14 @@ const NavLink = styled(Link)`
   font-weight: ${(props) => props.fontWeight || "normal"};
   line-height: 1;
   margin: 0 0.5rem 0 0;
+  padding: 0.25rem;
   text-decoration: none;
 
   &.current-page {
     border-bottom: 2px solid #222;
+  }
+  &:last-of-type {
+    margin-right: 0;
   }
 `;
 
@@ -23,7 +27,7 @@ const Header = () => (
       border-bottom: 1px solid #ddd;
       display: flex;
       justify-content: space-between;
-      padding: 0.5rem calc((100vw - 550px) / 2);
+      padding: 0.5rem calc((100vw - 550px - 0.5rem) / 2);
     `}
   >
     {/* site name */}
@@ -33,7 +37,7 @@ const Header = () => (
         margin-top: 0;
       `}
     >
-      <NavLink to="/" activeClassName="current-page" fontWeight="bold">
+      <NavLink to="/" fontWeight="bold">
         My Blog!
       </NavLink>
       <NavLink to="/" activeClassName="current-page">
